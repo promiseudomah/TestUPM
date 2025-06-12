9a18d92ca5b5009ec07ec6ce123b16c8f5209df5
Enwongo-Abasi Mfon Udomah <promzy132@gmail.com>
2025-06-12 19:54:07 +0100

    fix: changed color to yellow

--- UnityDevProject/Assets/TPromise/Runtime/Core/TestHello.cs
+++ UnityDevProject/Assets/TPromise/Runtime/Core/TestHello.cs
@@ -7,7 +7,7 @@ namespace TPromise
     {
         public void Run()
         {
-            Camera.main.backgroundColor = Color.red;
+            Camera.main.backgroundColor = Color.yellow;
             
             Canvas canvas = FindObjectOfType<Canvas>();
             if (canvas == null)
